1a23caec118364cd72a27bde7dc9d481768b89bd
thanchayawik <thanchayawik@gmail.com>
2024-07-18 20:58:46 +0700

    add text field and button

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class TodoList extends StatefulWidget {
-  const TodoList({Key? key}) : super(key: key);
+  TodoList({Key? key}) : super(key: key);
   static const nameRoute = '/todo_list';
   @override
   _TodoListState createState() => _TodoListState();
 }
 
 class _TodoListState extends State<TodoList> {
+  List<String> _todos = ['a', 'b', 'c'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +17,25 @@ class _TodoListState extends State<TodoList> {
         title: Text('Todo List'),
       ),
       body: Container(
-        child: Center(
-          child: Text("Todo List"),
+        child: Column(
+          children: [
+            Row(children: [
+              Expanded(flex: 2, child: TextField()),
+              Expanded(
+                  flex: 1,
+                  child: ElevatedButton(onPressed: () {}, child: Text('Add')))
+            ]),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_todos[index]),
+                  );
+                },
+                itemCount: _todos.length,
+              ),
+            )
+          ],
         ),
       ),
     );
